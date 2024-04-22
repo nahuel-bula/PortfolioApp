1aab7de4d10369f949ad1724abcc70e936d4f295
Nahuel Bula <nahuelbula@192.168.1.83>
2024-04-21 21:44:14 -0300

    feature/hero_section

--- portfolioApp/src/components/Button.jsx
+++ portfolioApp/src/components/Button.jsx
@@ -1,11 +1,11 @@
 import ButtonSVG from "../assets/svg/ButtonSvg"
 
 const Button = ({ className, href, onClick, children, px, white }) => {
-  const buttonClasses = `button relative infline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''} }`
+  const buttonClasses = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''} }`
   const spanClasses = "relative z-10"
 
   const renderButton = () => ( 
-  <button className={buttonClasses}>
+  <button className={buttonClasses} onClick={onClick}>
     <span className={spanClasses}>{children}</span>
     {ButtonSVG(white)}
   </button>)
